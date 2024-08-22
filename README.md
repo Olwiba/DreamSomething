b7a386d61863c720e6376935efb9b4a37b632f24
Olwiba <olwiba@gmail.com>
2024-08-22 18:59:07 +0100

    init project files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# DreamSomething
+# Dream Something 💭
+
+This nextJs project is a simple application that takes a user input, executes against a replicate model and returns generated image.
+
+## Getting Started 🚀
+
+1. Clone the repository
+2. Run `npm install` to install the dependencies
+3. Copy the `.env.example` file to `.env` and fill in the required values (obtain an API key from Replicate)
+4. Run `npm run dev` to start the development server
+5. Test the application!
---
+++ src/app/api/predictions/route.js
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
+ 
+// In production and preview deployments (on Vercel), the VERCEL_URL environment variable is set.
+// In development (on your local machine), the NGROK_HOST environment variable is set.
+const WEBHOOK_HOST = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : process.env.NGROK_HOST;
+ 
+export async function POST(request) {
+  if (!process.env.REPLICATE_API_TOKEN) {
+    throw new Error(
+      'The REPLICATE_API_TOKEN environment variable is not set. See README.md for instructions on how to set it.'
+    );
+  }
+ 
+  const { prompt } = await request.json();
+ 
+  const options = {
+    version: '7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc',
+    input: { prompt }
+  }
+ 
+  if (WEBHOOK_HOST) {
+    options.webhook = `${WEBHOOK_HOST}/api/webhooks`
+    options.webhook_events_filter = ["start", "completed"]
+  }
+ 
+  // A prediction is the result you get when you run a model, including the input, output, and other details
+  const prediction = await replicate.predictions.create(options);
+ 
+  if (prediction?.error) {
+    console.log('failed in prediction');
+    return NextResponse.json({ detail: prediction.error }, { status: 500 });
+  }
+ 
+  return NextResponse.json(prediction, { status: 201 });
+}
